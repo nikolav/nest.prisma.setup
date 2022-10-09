d927c85147273992bfff1e3054a4ea6d277b9fe8
Nikola Vukovic <nikolav@users.noreply.github.com>
2022-10-09 04:31:29 +0200

    Sun Oct  9 04:31:24 CEDT 2022

--- deploy-env.sh
+++ deploy-env.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sleep 1
-
 # update packages
 apt update
 apt-get update
@@ -25,12 +24,20 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-# if error @'apt-get update': 
+# if error @'apt-get update':
 #   try chmod a+r /etc/apt/keyrings/docker.gpg
 
 # install docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+sleep 1
+# allow ports
+ufw allow http
+ufw allow OpenSSH
+ufw allow 8081
+ufw allow 5555
+ufw enable
 
 # status check
 echo -n 'node: ' && node --version
@@ -39,3 +46,4 @@ echo -n 'yarn: ' && yarn --version
 docker --version
 docker-compose --version
 service docker status
+ufw status verbose
